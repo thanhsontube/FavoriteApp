54984a7f632b4ca9bd61799ce1e4fa7442714100
Son <thanhsontube@gmail.com>
2018-02-07 17:10:35 +0800

    (INIT) add dagger2

--- tslibrary/src/main/java/lab/ts/tslibrary/TsLibrary.kt
+++ tslibrary/src/main/java/lab/ts/tslibrary/TsLibrary.kt
@@ -1,6 +1,10 @@
 package lab.ts.tslibrary
 
+import dagger.android.AndroidInjector
+
 /**
  * Created by sonnt on 2/7/18.
  */
-data class TsLibrary(val name : String = "Hello")
+data class TsLibrary(val name : String = "Hello") {
+    var a : AndroidInjector<String>? = null
+}
