5db00ac79e80170f4bf6b2a68de4f27455ee7c7b
Son <thanhsontube@gmail.com>
2018-02-07 16:00:36 +0800

    (INIT) permission internet and add MainApp.

---
+++ app/src/main/java/lab/ts/favoriteapp/MainApplication.kt
@@ -0,0 +1,12 @@
+package lab.ts.favoriteapp
+
+import android.app.Application
+
+/**
+ * Created by sonnt on 2/7/18.
+ */
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
