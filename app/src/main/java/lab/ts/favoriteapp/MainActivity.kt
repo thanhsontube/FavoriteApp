755de3112b16af7f630f2812eb6fad979f825d99
Son <thanhsontube@gmail.com>
2018-02-07 17:16:10 +0800

    (INIT) rxjava2.

--- app/src/main/java/lab/ts/favoriteapp/MainActivity.kt
+++ app/src/main/java/lab/ts/favoriteapp/MainActivity.kt
@@ -2,7 +2,9 @@ package lab.ts.favoriteapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.d(">>>wowowoowowow")
+        Observable.just("Son").delay (1, TimeUnit.SECONDS)
+                .doOnNext { Timber.d(">>>after delay: $it") }
+                .subscribe()
     }
 }
