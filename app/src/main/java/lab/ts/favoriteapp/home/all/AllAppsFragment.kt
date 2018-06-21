a247fc2836deda9ba8be891fbfd4916be0dcb6e7
Son <thanhsontube@gmail.com>
2018-06-21 19:36:05 +0800

    Add debug mode.

--- app/src/main/java/lab/ts/favoriteapp/home/all/AllAppsFragment.kt
+++ app/src/main/java/lab/ts/favoriteapp/home/all/AllAppsFragment.kt
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import lab.ts.favoriteapp.R
 import lab.ts.favoriteapp.base.BaseFragment
 
@@ -16,10 +15,18 @@ import lab.ts.favoriteapp.base.BaseFragment
  */
 class AllAppsFragment : BaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_all_apps, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+//    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+//                              savedInstanceState: Bundle?): View? {
+//        // Inflate the layout for this fragment
+//        return inflater!!.inflate(R.layout.fragment_all_apps, container, false)
+//    }
 
 }
---
+++ debugmode/src/main/java/lab/ts/debugmode/MainActivity.kt
@@ -0,0 +1,12 @@
+package lab.ts.debugmode
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MyMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_activity_main)
+    }
+}
