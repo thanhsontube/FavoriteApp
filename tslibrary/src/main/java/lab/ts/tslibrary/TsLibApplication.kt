7c5cfe85334cdd0d23dc04884e88435e01981cc2
Son <thanhsontube@gmail.com>
2018-06-22 13:05:48 +0800

    Clean libs.

--- tslibrary/src/main/java/lab/ts/tslibrary/TsLibApplication.kt
+++ tslibrary/src/main/java/lab/ts/tslibrary/TsLibApplication.kt
@@ -6,19 +6,19 @@ import timber.log.Timber
 /**
  * Created by sonnt on 2/7/18.
  */
-open class TsLibApplication : Application(), ThirdParty {
+open class TsLibApplication : Application(), ICommonLibs {
     override fun onCreate() {
         super.onCreate()
-
         initTimber()
         Timber.d(">>>TsLibApplication onCreate")
     }
 
     override fun initTimber() {
+        Timber.d(">>>initTimber")
         Timber.plant(Timber.DebugTree())
     }
 }
 
-interface ThirdParty {
+interface ICommonLibs {
     fun initTimber()
 }
